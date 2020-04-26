69c655a8f5b4606b08c1a8dd025043ccf6300db9
Esteban Caparroz <estcap2@gmail.com>
2020-04-26 14:10:08 -0300

    reimplementation of savestates for independence of slots

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/scenes/LoadSaveScene.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/scenes/LoadSaveScene.java
@@ -28,15 +28,29 @@ import com.shatteredpixel.shatteredpixeldungeon.GamesInProgress;
 import com.shatteredpixel.shatteredpixeldungeon.ShatteredPixelDungeon;
 import com.shatteredpixel.shatteredpixeldungeon.Statistics;
 import com.shatteredpixel.shatteredpixeldungeon.actors.Actor;
+import com.shatteredpixel.shatteredpixeldungeon.actors.hero.Hero;
 import com.shatteredpixel.shatteredpixeldungeon.actors.hero.HeroSubClass;
 import com.shatteredpixel.shatteredpixeldungeon.actors.mobs.Mob;
+import com.shatteredpixel.shatteredpixeldungeon.actors.mobs.npcs.Blacksmith;
+import com.shatteredpixel.shatteredpixeldungeon.actors.mobs.npcs.Ghost;
+import com.shatteredpixel.shatteredpixeldungeon.actors.mobs.npcs.Imp;
+import com.shatteredpixel.shatteredpixeldungeon.actors.mobs.npcs.Wandmaker;
+import com.shatteredpixel.shatteredpixeldungeon.items.Generator;
+import com.shatteredpixel.shatteredpixeldungeon.items.Item;
+import com.shatteredpixel.shatteredpixeldungeon.items.potions.Potion;
+import com.shatteredpixel.shatteredpixeldungeon.items.rings.Ring;
+import com.shatteredpixel.shatteredpixeldungeon.items.scrolls.Scroll;
 import com.shatteredpixel.shatteredpixeldungeon.journal.Journal;
+import com.shatteredpixel.shatteredpixeldungeon.journal.Notes;
 import com.shatteredpixel.shatteredpixeldungeon.levels.Level;
+import com.shatteredpixel.shatteredpixeldungeon.levels.rooms.secret.SecretRoom;
+import com.shatteredpixel.shatteredpixeldungeon.levels.rooms.special.SpecialRoom;
 import com.shatteredpixel.shatteredpixeldungeon.messages.Messages;
 import com.shatteredpixel.shatteredpixeldungeon.ui.Archs;
 import com.shatteredpixel.shatteredpixeldungeon.ui.ExitButton;
 import com.shatteredpixel.shatteredpixeldungeon.ui.GameLog;
 import com.shatteredpixel.shatteredpixeldungeon.ui.Icons;
+import com.shatteredpixel.shatteredpixeldungeon.ui.QuickSlotButton;
 import com.shatteredpixel.shatteredpixeldungeon.ui.RedButton;
 import com.shatteredpixel.shatteredpixeldungeon.ui.RenderedTextBlock;
 import com.shatteredpixel.shatteredpixeldungeon.ui.Window;
@@ -47,18 +61,22 @@ import com.shatteredpixel.shatteredpixeldungeon.windows.WndOptions;
 import com.shatteredpixel.shatteredpixeldungeon.windows.WndStartGame;
 import com.watabou.noosa.BitmapText;
 import com.watabou.noosa.Camera;
+import com.watabou.noosa.Game;
 import com.watabou.noosa.Image;
 import com.watabou.noosa.NinePatch;
 import com.watabou.noosa.ui.Button;
+import com.watabou.utils.Bundlable;
 import com.watabou.utils.Bundle;
 import com.watabou.utils.FileUtils;
+import com.watabou.utils.SparseArray;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class LoadSaveScene extends PixelScene {
 
-	private static final String GAME_FOLDER = "savegame%d";
+	private static final String GAME_FOLDER = "game%d";
 	private static final String GAME_FILE	= "game.dat";
 	private static final String DEPTH_FILE	= "depth%d.dat";
 
@@ -66,19 +84,34 @@ public class LoadSaveScene extends PixelScene {
 	private static final int SLOT_HEIGHT = 30;
 	private static int BTN_WIDTH = 50;
 	private static int BTN_HEIGHT = 15;
+
+	private static final String VERSION		= "version";
+	private static final String SEED		= "seed";
+	private static final String CHALLENGES	= "challenges";
+	private static final String HERO		= "hero";
+	private static final String GOLD		= "gold";
+	private static final String DEPTH		= "depth";
+	private static final String DROPPED     = "dropped%d";
+	private static final String PORTED      = "ported%d";
+	private static final String LEVEL		= "level";
+	private static final String LIMDROPS    = "limited_drops";
+	private static final String CHAPTERS	= "chapters";
+	private static final String QUESTS		= "quests";
+	private static final String BADGES		= "badges";
+
 	@Override
 	public void create() {
 		super.create();
 
 		uiCamera.visible = false;
-		
+
 		int w = Camera.main.width;
 		int h = Camera.main.height;
-		
+
 		Archs archs = new Archs();
 		archs.setSize( w, h );
 		add( archs );
-		
+
 		RenderedTextBlock title = PixelScene.renderTextBlock( Messages.get(this, "title"), 9);
 		title.hardlight(Window.TITLE_COLOR);
 		title.setPos(
@@ -122,41 +155,54 @@ public class LoadSaveScene extends PixelScene {
 		add(btnSave1);
 
 		fadeIn();
-		
+
 	}
 
+	/**
+	 * CLick on Save State
+	 * @param slot
+	 * @throws IOException
+	 */
 	private void save(int slot) throws IOException {
-		slot = slot + 100;
 		//TODO. ADD OVERWRITE WARNING
 		saveSavegame(slot);
 		InterlevelScene.mode = InterlevelScene.Mode.CONTINUE;
 		ShatteredPixelDungeon.switchScene(InterlevelScene.class);
 	}
 
+	/**
+	 * Save into slot
+	 * @param slot
+	 * @throws IOException
+	 */
 	private void saveSavegame(int slot) throws IOException{
 		if (Dungeon.hero != null && Dungeon.hero.isAlive()) {
 			Actor.fixTime();
-			Dungeon.saveGame( slot );
-			Dungeon.saveLevel( slot );
+			saveStateGame( slot );
+			saveStateLevel( slot );
 		}
 	}
 
 	private void loadSavegame(int slot) throws  IOException{
 		Mob.clearHeldAllies();
 		GameLog.wipe();
-		Dungeon.loadGame( slot , true);
+		loadStateGame( slot , true);
 		if (Dungeon.depth == -1) {
 			Dungeon.depth = Statistics.deepestFloor;
-			Dungeon.switchLevel( Dungeon.loadLevel( slot ), -1 );
+			Dungeon.switchLevel( loadStateLevel( slot ), -1 );
 		} else {
-			Level level = Dungeon.loadLevel( slot );
+			Level level = loadStateLevel( slot );
 			Dungeon.switchLevel( level, Dungeon.hero.pos );
 		}
 	}
 
+	/**
+	 * Click on Load State
+	 * @param slot
+	 * @throws IOException
+	 */
 	private void load (int slot) throws IOException {
-		int finalslot = slot + 100;
-		Bundle bundle = FileUtils.bundleFromFile( GamesInProgress.gameFile( finalslot ) );
+		Bundle bundle = FileUtils.bundleFromFile( stateFile( slot ) );
 		long seed = bundle.contains( "seed" ) ? bundle.getLong( "seed" ) : DungeonSeed.randomSeed();
 		if(Dungeon.seed != seed){
 			ShatteredPixelDungeon.scene().add(new WndOptions(
@@ -168,7 +214,7 @@ public class LoadSaveScene extends PixelScene {
 				protected void onSelect( int index ){
 					if (index == 0) {
 						try {
-							loadSavegame(finalslot);
+							loadSavegame(slot);
 							Dungeon.saveAll();
 							InterlevelScene.mode = InterlevelScene.Mode.CONTINUE;
 							ShatteredPixelDungeon.switchScene(InterlevelScene.class);
@@ -179,11 +225,219 @@ public class LoadSaveScene extends PixelScene {
 				}
 			} );
 		} else {
-			loadSavegame(finalslot);
+			loadSavegame(slot);
 			Dungeon.saveAll();
 			InterlevelScene.mode = InterlevelScene.Mode.CONTINUE;
 			ShatteredPixelDungeon.switchScene(InterlevelScene.class);
 		}
+	}
+
+	private void saveStateGame( int save ) {
+		try {
+			Bundle bundle = new Bundle();
+
+			int version = Game.versionCode;
+			bundle.put( VERSION, version );
+			bundle.put( SEED, Dungeon.seed );
+			bundle.put( CHALLENGES, Dungeon.challenges );
+			bundle.put( HERO, Dungeon.hero );
+			bundle.put( GOLD, Dungeon.gold );
+			bundle.put( DEPTH, Dungeon.depth );
+
+			for (int d : Dungeon.droppedItems.keyArray()) {
+				bundle.put(Messages.format(DROPPED, d), Dungeon.droppedItems.get(d));
+			}
+
+			for (int p :  Dungeon.portedItems.keyArray()){
+				bundle.put(Messages.format(PORTED, p),  Dungeon.portedItems.get(p));
+			}
+
+			Dungeon.quickslot.storePlaceholders( bundle );
+
+			Bundle limDrops = new Bundle();
+			Dungeon.LimitedDrops.store( limDrops );
+			bundle.put ( LIMDROPS, limDrops );
+
+			int count = 0;
+			int ids[] = new int[ Dungeon.chapters.size()];
+			for (Integer id :  Dungeon.chapters) {
+				ids[count++] = id;
+			}
+			bundle.put( CHAPTERS, ids );
+
+			Bundle quests = new Bundle();
+			Ghost.Quest.storeInBundle( quests );
+			Wandmaker.Quest.storeInBundle( quests );
+			Blacksmith.Quest.storeInBundle( quests );
+			Imp.Quest.storeInBundle( quests );
+			bundle.put( QUESTS, quests );
+
+			SpecialRoom.storeRoomsInBundle( bundle );
+			SecretRoom.storeRoomsInBundle( bundle );
+
+			Statistics.storeInBundle( bundle );
+			Notes.storeInBundle( bundle );
+			Generator.storeInBundle( bundle );
+
+			Scroll.save( bundle );
+			Potion.save( bundle );
+			Ring.save( bundle );
+
+			Actor.storeNextID( bundle );
+
+			Bundle badges = new Bundle();
+			Badges.saveLocal( badges );
+			bundle.put( BADGES, badges );
+
+			FileUtils.bundleToFile( stateFile(save), bundle);
+
+		} catch (IOException e) {
+			GamesInProgress.setUnknown( save );
+			ShatteredPixelDungeon.reportException(e);
+		}
+	}
+
+	private void saveStateLevel( int save ) throws IOException {
+		Bundle bundle = new Bundle();
+		bundle.put( LEVEL, Dungeon.level );
+
+		FileUtils.bundleToFile(stateDepthFile( save, Dungeon.depth), bundle);
+	}
+
+	private void loadStateGame( int save, boolean fullLoad ) throws IOException {
+
+		Bundle bundle = FileUtils.bundleFromFile( stateFile( save ) );
+
+		Dungeon.version = bundle.getInt( VERSION );
+
+		Dungeon.seed = bundle.contains( SEED ) ? bundle.getLong( SEED ) : DungeonSeed.randomSeed();
+
+		Actor.restoreNextID( bundle );
+
+		Dungeon.quickslot.reset();
+		QuickSlotButton.reset();
+
+		Dungeon.challenges = bundle.getInt( CHALLENGES );
+
+		Dungeon.level = null;
+		Dungeon.depth = -1;
+
+		Scroll.restore( bundle );
+		Potion.restore( bundle );
+		Ring.restore( bundle );
+
+		Dungeon.quickslot.restorePlaceholders( bundle );
+
+		if (fullLoad) {
+
+			Dungeon.LimitedDrops.restore( bundle.getBundle(LIMDROPS) );
+
+			Dungeon.chapters = new HashSet<>();
+			int ids[] = bundle.getIntArray( CHAPTERS );
+			if (ids != null) {
+				for (int id : ids) {
+					Dungeon.chapters.add( id );
+				}
+			}
+
+			Bundle quests = bundle.getBundle( QUESTS );
+			if (!quests.isNull()) {
+				Ghost.Quest.restoreFromBundle( quests );
+				Wandmaker.Quest.restoreFromBundle( quests );
+				Blacksmith.Quest.restoreFromBundle( quests );
+				Imp.Quest.restoreFromBundle( quests );
+			} else {
+				Ghost.Quest.reset();
+				Wandmaker.Quest.reset();
+				Blacksmith.Quest.reset();
+				Imp.Quest.reset();
+			}
+
+			SpecialRoom.restoreRoomsFromBundle(bundle);
+			SecretRoom.restoreRoomsFromBundle(bundle);
+		}
+
+		Bundle badges = bundle.getBundle(BADGES);
+		if (!badges.isNull()) {
+			Badges.loadLocal( badges );
+		} else {
+			Badges.reset();
+		}
+
+		Notes.restoreFromBundle( bundle );
+
+		Dungeon.hero = null;
+		Dungeon.hero = (Hero)bundle.get( HERO );
+
+		//pre-0.7.0 saves, back when alchemy had a window which could store items
+		if (bundle.contains("alchemy_inputs")){
+			for (Bundlable item : bundle.getCollection("alchemy_inputs")){
+
+				//try to add normally, force-add otherwise.
+				if (!((Item)item).collect(Dungeon.hero.belongings.backpack)){
+					Dungeon.hero.belongings.backpack.items.add((Item)item);
+				}
+			}
+		}
+
+		Dungeon.gold = bundle.getInt( GOLD );
+		Dungeon.depth = bundle.getInt( DEPTH );
+
+		Statistics.restoreFromBundle( bundle );
+		Generator.restoreFromBundle( bundle );
+
+		Dungeon.droppedItems = new SparseArray<>();
+		Dungeon.portedItems = new SparseArray<>();
+		for (int i=1; i <= 26; i++) {
+
+			//dropped items
+			ArrayList<Item> items = new ArrayList<>();
+			if (bundle.contains(Messages.format( DROPPED, i )))
+				for (Bundlable b : bundle.getCollection( Messages.format( DROPPED, i ) ) ) {
+					items.add( (Item)b );
+				}
+			if (!items.isEmpty()) {
+				Dungeon.droppedItems.put( i, items );
+			}
+
+			//ported items
+			items = new ArrayList<>();
+			if (bundle.contains(Messages.format( PORTED, i )))
+				for (Bundlable b : bundle.getCollection( Messages.format( PORTED, i ) ) ) {
+					items.add( (Item)b );
+				}
+			if (!items.isEmpty()) {
+				Dungeon.portedItems.put( i, items );
+			}
+		}
+	}
+
+	private static Level loadStateLevel( int save ) throws IOException {
+
+		Dungeon.level = null;
+		Actor.clear();
+
+		Bundle bundle = FileUtils.bundleFromFile( stateDepthFile( save, Dungeon.depth)) ;
+
+		Level level = (Level)bundle.get( LEVEL );
+
+		if (level == null){
+			throw new IOException();
+		} else {
+			return level;
+		}
+	}
+
+	private static String stateDepthFile( int slot, int depth ) {
+		return stateFolder(slot) + "/" + Messages.format(DEPTH_FILE, depth);
+	}
+
+	private String stateFile(int slot){
+		return stateFolder(slot) + "/" + GAME_FILE;
+	}
+
+	private static String stateFolder( int slot ){
+		return GamesInProgress.gameFolder(GamesInProgress.curSlot) + "/savestate" + slot;
 	}
 
 	@Override
@@ -191,153 +445,4 @@ public class LoadSaveScene extends PixelScene {
 		InterlevelScene.mode = InterlevelScene.Mode.CONTINUE;
 		ShatteredPixelDungeon.switchScene(InterlevelScene.class);
 	}
-
-
-	/*
-	private static class SaveSlotButton extends Button {
-		
-		private NinePatch bg;
-		
-		private Image hero;
-		private RenderedTextBlock name;
-		
-		private Image steps;
-		private BitmapText depth;
-		private Image classIcon;
-		private BitmapText level;
-		
-		private int slot;
-		private boolean newGame;
-		
-		@Override
-		protected void createChildren() {
-			super.createChildren();
-			
-			bg = Chrome.get(Chrome.Type.GEM);
-			add( bg);
-			
-			name = PixelScene.renderTextBlock(9);
-			add(name);
-		}
-		
-		public void set( int slot ){
-			this.slot = slot;
-			GamesInProgress.Info info = GamesInProgress.check(slot);
-			newGame = info == null;
-			if (newGame){
-				name.text( Messages.get(LoadSaveScene.class, "new"));
-				
-				if (hero != null){
-					remove(hero);
-					hero = null;
-					remove(steps);
-					steps = null;
-					remove(depth);
-					depth = null;
-					remove(classIcon);
-					classIcon = null;
-					remove(level);
-					level = null;
-				}
-			} else {
-				
-				if (info.subClass != HeroSubClass.NONE){
-					name.text(Messages.titleCase(info.subClass.title()));
-				} else {
-					name.text(Messages.titleCase(info.heroClass.title()));
-				}
-				
-				if (hero == null){
-					hero = new Image(info.heroClass.spritesheet(), 0, 15*info.armorTier, 12, 15);
-					add(hero);
-					
-					steps = new Image(Icons.get(Icons.DEPTH));
-					add(steps);
-					depth = new BitmapText(PixelScene.pixelFont);
-					add(depth);
-					
-					classIcon = new Image(Icons.get(info.heroClass));
-					add(classIcon);
-					level = new BitmapText(PixelScene.pixelFont);
-					add(level);
-				} else {
-					hero.copy(new Image(info.heroClass.spritesheet(), 0, 15*info.armorTier, 12, 15));
-					
-					classIcon.copy(Icons.get(info.heroClass));
-				}
-				
-				depth.text(Integer.toString(info.depth));
-				depth.measure();
-				
-				level.text(Integer.toString(info.level));
-				level.measure();
-				
-				if (info.challenges > 0){
-					name.hardlight(Window.TITLE_COLOR);
-					depth.hardlight(Window.TITLE_COLOR);
-					level.hardlight(Window.TITLE_COLOR);
-				} else {
-					name.resetColor();
-					depth.resetColor();
-					level.resetColor();
-				}
-				
-			}
-			
-			layout();
-		}
-		
-		@Override
-		protected void layout() {
-			super.layout();
-			
-			bg.x = x;
-			bg.y = y;
-			bg.size( width, height );
-			
-			if (hero != null){
-				hero.x = x+8;
-				hero.y = y + (height - hero.height())/2f;
-				align(hero);
-				
-				name.setPos(
-						hero.x + hero.width() + 6,
-						y + (height - name.height())/2f
-				);
-				align(name);
-				
-				classIcon.x = x + width - 24 + (16 - classIcon.width())/2f;
-				classIcon.y = y + (height - classIcon.height())/2f;
-				align(classIcon);
-				
-				level.x = classIcon.x + (classIcon.width() - level.width()) / 2f;
-				level.y = classIcon.y + (classIcon.height() - level.height()) / 2f + 1;
-				align(level);
-				
-				steps.x = x + width - 40 + (16 - steps.width())/2f;
-				steps.y = y + (height - steps.height())/2f;
-				align(steps);
-				
-				depth.x = steps.x + (steps.width() - depth.width()) / 2f;
-				depth.y = steps.y + (steps.height() - depth.height()) / 2f + 1;
-				align(depth);
-				
-			} else {
-				name.setPos(
-						x + (width - name.width())/2f,
-						y + (height - name.height())/2f
-				);
-				align(name);
-			}
-			
-			
-		}
-		
-		@Override
-		protected void onClick() {
-			ShatteredPixelDungeon.scene().add( new WndLoadSave(slot));
-		}
-	}
-
-	*/
 }
